a4d586fc6a7e27bfc6675d2f7c4ab908f1e9c950
Andrey <erkcasb@gmail.com>
2021-04-21 16:48:46 +0500

    Initial commit

---
+++ src/main/java/netology/mvc/service/PostService.java
@@ -0,0 +1,33 @@
+package netology.mvc.service;
+
+import netology.mvc.exception.NotFoundException;
+import netology.mvc.model.Post;
+import org.springframework.stereotype.Service;
+import netology.mvc.repository.PostRepository;
+
+import java.util.List;
+
+@Service
+public class PostService {
+    private final PostRepository repository;
+
+    public PostService(PostRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<Post> all() {
+        return repository.all();
+    }
+
+    public Post getById(long id) {
+        return repository.getById(id).orElseThrow(NotFoundException::new);
+    }
+
+    public Post save(Post post) {
+        return repository.save(post);
+    }
+
+    public void removeById(long id) {
+        repository.removeById(id);
+    }
+}
